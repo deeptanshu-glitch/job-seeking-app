f4b4a42c988c381c7f342d326ad711512d3a831e
Deeptanshu <deeptanshu1203@gmail.com>
2026-01-24 03:09:27 +0530

    dashboard making1

--- react-jobseekingapp/src/dashboard.jsx
+++ react-jobseekingapp/src/dashboard.jsx
@@ -1,13 +1,14 @@
 
 import React from "react";
 import CollapsibleExample from "./navafter";
+import ControlledCarousel from "./carousel";
 import "./dashboard.css";
 
 function Dash() {
   return (
     <div className="dashboard">
        < CollapsibleExample />
-       
+       <ControlledCarousel />
 
       
     </div>
--- react-jobseekingapp/src/navafter.jsx
+++ react-jobseekingapp/src/navafter.jsx
@@ -1,11 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-transparent" variant='light'>
+    <Navbar collapseOnSelect expand="lg" className="bg-transparent" variant='light'style={{'border-bottom': '1px solid rgba(0, 0, 0, 0.4)'}}>
       <Container>
         <Navbar.Brand >🔍Job Finder</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -16,14 +16,32 @@ function CollapsibleExample() {
             <Nav.Link href="/Industry">Industry</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/profile">profile</Nav.Link>
-            <Nav.Link eventKey={2} href="/signout">
-              Signout
+            <Nav.Link href="/post" style={
+              {
+                backgroundColor: 'red',
+                borderRadius: '15px',
+                color: 'white',
+                margin: '4px',
+               animation: 'pulse 2s infinite'
+              }
+              }>
+              Post a Job
             </Nav.Link>
+            <Nav.Link href="/profile" style={
+              {
+                backgroundColor: 'blue',
+                borderRadius: '15px',
+                color: 'white',
+                margin: '1px',
+               animation: 'pulse 2s infinite'
+              }
+            }
+              >profile</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+    </Navbar >
   );
 }
 
