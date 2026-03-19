16c1b2d1ecaa853eab5da010ece9a3ccf9299e8b
Deeptanshu <deeptanshu1203@gmail.com>
2026-03-19 19:32:00 +0530

    css corrected

--- react-jobseekingapp/src/initial.jsx
+++ react-jobseekingapp/src/initial.jsx
@@ -9,7 +9,7 @@ function Welcome(){
     return(
         <div className="intro">
         <div className="intro-bg">
-            <h1 className="dropBounce heading m-2 m-sm-3 m-md-4 m-lg-5 delay-0"><br /><br />JOB SEEKER<br /><br /></h1>
+         <h1 className="dropBounce heading m-0 m-sm-0 mx-5 mx-lg-5 delay-0"><br /><br />JOB SEEKER<br /><br /></h1>
             <h4 className="dropBounce Quotes delay-1"><br /><br /> Less stress,<br /></h4>
             <h4 className="dropBounce Quotes delay-2"> Better opportunities, <br /></h4>
             <h4 className="dropBounce Quotes delay-3">Smarter career decisions<br /><br /> </h4>
